cba5370785cced39af319fa089714227503e711a
sreetamadas <sreetama.d2005@gmail.com>
2018-05-01 12:51:48 +0530

    Create python_plot.py

---
+++ python_plot.py
@@ -0,0 +1,27 @@
+## plotting methods
+
+import pandas as pd
+import matplotlib.pyplot as plt
+
+
+df = pd.read_csv(filename)
+# check datatype of the datetime column
+print(df.dtypes)
+# set it as datetime
+df = pd.to_datetime(df['dateTime'])    # df['dateTime'] == df.dateTime
+
+# set the datetime col as index for plotting
+df = df.set_index('dateTime')
+
+# see list of available styles for plots (font, line & point types)
+print(plt.style.available)
+# set style
+plt.style.use('fivethirtyeight')
+
+# describe graph labels
+ax = df.plot(color="blue")
+ax.set_xlabel('Date')
+ax.set_ylabel('dependent variable Y')
+ax.set_title('Y vs time')
+plt.show()
+
