e2d1c7508c45c7c6ed44a0a53dced003b4903153
sreetamadas <sreetama.d2005@gmail.com>
2018-05-01 22:34:22 +0530

    Update check_timeSeries_aggregate_MA.py

--- check_timeSeries_aggregate_MA.py
+++ check_timeSeries_aggregate_MA.py
@@ -3,8 +3,12 @@
 ##  2. remove short term fluctuations
 ##  3. remove outliers
 
-df_mean = df.rolling(window=7).mean()   # set window according to seasonality
+# df
+# dateTime    Y
+# 2018-01-01  398
+# ...
 
+df_mean = df.rolling(window=7).mean()   # set window according to seasonality
 
 ## compute aggregates to understand weekly/ monthly/ yearly means
 # eg find mean values during each month of the year
@@ -13,3 +17,14 @@ index_month = df.index.month
 Y_by_month = df.groupby(index_month).mean()
 Y_by_month.plot()
 plt.show()
+
+
+## compute bouunds from MA 
+# MA
+ma = df.rolling(window=52).mean()
+# Compute the 52 weeks rolling standard deviation of the DataFrame
+mstd = df.rolling(window=52).std()
+# Add the upper bound column to the ma DataFrame
+ma['upper'] = ma['co2'] + (2 * mstd['Y'])
+# Add the lower bound column to the ma DataFrame
+ma['lower'] = ma['co2'] - (2 * mstd['Y'])
