6157207e647b41bb8fb0f01fa3fb88355adaef0a
sreetamadas <sreetama.d2005@gmail.com>
2018-05-01 13:43:29 +0530

    Update python_plot.py

--- python_plot.py
+++ python_plot.py
@@ -13,19 +13,27 @@ df = pd.to_datetime(df['dateTime'])    # df['dateTime'] == df.dateTime
 # set the datetime col as index for plotting
 df = df.set_index('dateTime')
 
+
 # see list of available styles for plots (font, line & point types)
 print(plt.style.available)
 # set style
 plt.style.use('fivethirtyeight')
+
 
 # describe graph labels
 ax = df.plot(color="blue", figsize=(8,3), linewidth=2, fontsize=6)
 ax.set_xlabel('Date')
 ax.set_ylabel('dependent variable Y')
 ax.set_title('Y vs time')
+
 # add horizontal & vertical lines
 ax.axvline(x='2017-12-25', color='red', linestyle='--')
 ax.axhline(y=100, color='green', linestyle='--')
+
+# highlight a region
+ax.axvspan('2017-12-01', '2018-01-01', color='red', alpha=0.3)
+ax.axhspan(8, 6, color='green', alpha=0.3)
+
 # show the plot
 plt.show()
 
