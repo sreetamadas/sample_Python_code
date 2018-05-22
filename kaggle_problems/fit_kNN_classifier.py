25b8cbdd4be84f61d30dfbaa47618e3f9e46f197
sreetamadas <sreetama.d2005@gmail.com>
2018-05-22 22:47:50 +0530

    Update fit_kNN_classifier.py

--- kaggle_problems/fit_kNN_classifier.py
+++ kaggle_problems/fit_kNN_classifier.py
@@ -1,8 +1,14 @@
 from sklearn.neighbors import KNeighborsClassifier
 
 knn = KNeighborsClassifier(n_neighbors = 6)
+
 # X is the df of features, Y is the target variable value df
+y = df['party'].values
+X = df.drop('party', axis=1).values
+
+# fit model on training data
 knn.fit(X, Y)  # the input features should be continuous & not categorical
 
+# predict on validation/ test data
 prediction = knn.predict(X_new)
 print('Prediction {}’.format(prediction))
