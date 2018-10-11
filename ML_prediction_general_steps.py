36afc6d6391ec778d2831d4eb7fa24c305592672
sreetamadas <sreetama.d2005@gmail.com>
2018-10-11 12:25:41 +0530

    Update ML_prediction_general_steps.py

--- ML_prediction_general_steps.py
+++ ML_prediction_general_steps.py
@@ -22,6 +22,8 @@ df[['class','class2']] = df[['class','class2']].astype('category')
 X = df[['X1','X2','X3','X4','X6']]  
 y = df[['class2']]   
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=0)
+# use "stratify" option for consistent class distribution between training and test sets
+# https://towardsdatascience.com/fine-tuning-a-classifier-in-scikit-learn-66e048c21e65
 
 
 # 7. normalize the data
@@ -29,3 +31,14 @@ scaler = MinMaxScaler()
 X_train_scaled = scaler.fit_transform(X_train)
 X_test_scaled = scaler.transform(X_test)
 #X_train_scaled.shape
+
+
+# 8. use PCA, instead of actual features, if too many features (or, do a feature importance map)
+pca = decomposition.PCA(n_components=2)
+pca.fit(X_train)
+X_train_pca = pca.transform(X_train)
+X_test_pca = pca.transform(X_test)
+print((pca.explained_variance_ratio_)*100)
+
+
+
