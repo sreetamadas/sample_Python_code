5f4653663d1d1744bb7c55b2a8b3c1babcad3433
sreetamadas <sreetama.d2005@gmail.com>
2018-10-12 10:24:17 +0530

    Update create_FFT.py

--- create_FFT.py
+++ create_FFT.py
@@ -1,3 +1,24 @@
 # https://www.researchgate.net/post/Can_someone_provide_me_the_Python_script_to_plot_FFT
 
+import numpy as np
+import scipy.signal as signal
+import matplotlib.pyplot as plt
+
+def plotSpectrum(y,Fs):
+    n = int(len(y)) # length of the signal
+    k = np.arange(n)
+    T = n/Fs  # Fs is sampling freq. , T will be time duration of signal
+    frq = k/T # two sides frequency range
+    frq = frq[range(int(len(frq)/2))] # one side frequency range
+    Y = np.fft.fftn(y)/n # fft computing and normalization
+    Y = Y[range(int(len(Y)/2))]
+    
+    fig, ax = plt.subplots(figsize=(15,3))
+    ax.scatter(frq,abs(Y),marker = 'o', s=10, alpha=0.4) #, cmap = cm.jet ; marker = 'o'\n",
+    
+    return frq[np.argmax(abs(Y))]
+
+ 
+# np.argmax Returns the index(ices) 'I' of the maximum values of 'abs(Y)'
+# frq[..] returns the value at index I in array frq - this corresponds to the no. of cycles in the signal y
 
