d0954da3bc138d3f00bc1748510e205492ef096b
sreetamadas <sreetama.d2005@gmail.com>
2018-10-17 12:37:34 +0530

    Update dataFrame_manipulations.py

--- dataFrame_manipulations.py
+++ dataFrame_manipulations.py
@@ -1,13 +1,41 @@
-dataframe manipulations,
+#####  dataframe manipulations  #####
 
-concatenation,merge,
 
-array (row) to df, 
+####  concatenation  ####
+# ADD 1 df below the other
+newdata = pd.DataFrame()
+df = pd.DataFrame()
+# ... intermediate steps with data manipulations ....
+newdata = pd.concat([newdata,df],axis=0)  # axis=0 means concatenating by row
 
-append, extend
+## ADD dataframes by column
+newdata = pd.DataFrame()
+df = pd.DataFrame()
+# ... intermediate steps with data manipulations ....
+newdata = pd.concat([newdata,df],axis=1)  # axis=1 means concatenating by column
+
+
+
+####  merge  : inner join, outer join, etc
+
+
+
+
+#### array (row) to df, 
+df=pd.DataFrame(array).T     # T implies taking transpose
+
+
+####   append, extend
+
+
+
 
 loc & iloc;
 
-passing df row to a function through loop
 
+
+####   passing df row to a function through loop
+for i in range(0,df.shape[0]):  
+    total=0
+    X_score = user_defined_python_function(df.loc[i, 'columnX'])
 
