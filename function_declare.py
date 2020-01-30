567c854a36a5799539018d2ede9c3f7688f53956
sreetamadas <sreetama.d2005@gmail.com>
2020-01-30 18:03:36 +0530

    Create function_declare.py

---
+++ function_declare.py
@@ -0,0 +1,20 @@
+##### declaring functions #####
+ def function_name (comma-separated list of arguments):
+	"optional documentation string"
+	block of code
+
+#def addMe2Me (x):
+#	"apply + operation to argument"
+#	return (x + x)
+#x = [1, 'a']  # 'abc'
+#y = addMe2Me(x)
+#print y
+
+
+#####################################################################################
+
+## some built-in functions ###
+
+# range([start,]stop[,step])	
+# gives list of integers, begin at 'start' up to (but not including) 'stop' in increments of 'step'; default => start=0, step = 1
+
