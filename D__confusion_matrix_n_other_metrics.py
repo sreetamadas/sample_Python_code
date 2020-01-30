1c99fff347f1409bb9e4756a480d211fa5c629da
sreetamadas <sreetama.d2005@gmail.com>
2020-01-31 00:44:02 +0530

    Update D__confusion_matrix_n_other_metrics.py

--- D__confusion_matrix_n_other_metrics.py
+++ D__confusion_matrix_n_other_metrics.py
@@ -94,3 +94,83 @@ spec = p[1][0]
 print('sen: ' + str(sen) +' , spec: ' + str(spec))
 
 
+
+
+
+#######################################################################
+#######  change threshold : interpretation of the following code is difficult ########
+# https://towardsdatascience.com/fine-tuning-a-classifier-in-scikit-learn-66e048c21e65
+
+
+### this is for the svc grid search
+#y_scores = grid_search_clf.decision_function(X_test) # .predict_proba(X_test)[:, 1]
+# for optimised SVC
+y_scores = model_svc_opt.decision_function(X_test_scaled)
+
+from sklearn.metrics import precision_recall_curve
+p, r, thresholds = precision_recall_curve(y_test, y_scores)
+
+def adjusted_classes(y_scores, t):
+    """
+    This function adjusts class predictions based on the prediction threshold (t).
+    Will only work for binary classification problems.
+    """
+    return [1 if y >= t else 0 for y in y_scores]
+
+
+def precision_recall_threshold(p, r, thresholds, t=0.5):
+    """
+    plots the precision recall curve and shows the current value for each
+    by identifying the classifier's threshold (t).
+    """
+    
+    # generate new class predictions based on the adjusted_classes
+    # function above and view the resulting confusion matrix.
+    y_pred_adj = adjusted_classes(y_scores, t)
+    print(pd.DataFrame(confusion_matrix(y_test, y_pred_adj),
+                       columns=['pred_neg', 'pred_pos'], 
+                       index=['neg', 'pos']))
+    
+    # plot the curve
+    plt.figure(figsize=(8,8))
+    plt.title("Precision and Recall curve ^ = current threshold")
+    plt.step(r, p, color='b', alpha=0.2,
+             where='post')
+    plt.fill_between(r, p, step='post', alpha=0.2,
+                     color='b')
+    plt.ylim([0.5, 1.01]);
+    plt.xlim([0.5, 1.01]);
+    plt.xlabel('Recall');
+    plt.ylabel('Precision');
+    
+    # plot the current threshold on the line
+    close_default_clf = np.argmin(np.abs(thresholds - t))
+    plt.plot(r[close_default_clf], p[close_default_clf], '^', c='k',
+            markersize=15)
+
+
+precision_recall_threshold(p, r, thresholds, 0.30)
+
+
+
+def plot_precision_recall_vs_threshold(precisions, recalls, thresholds):
+    """
+    Modified from:
+    Hands-On Machine learning with Scikit-Learn
+    and TensorFlow; p.89
+    """
+    plt.figure(figsize=(8, 8))
+    plt.title("Precision and Recall Scores as a function of the decision threshold")
+    plt.plot(thresholds, precisions[:-1], "b--", label="Precision")
+    plt.plot(thresholds, recalls[:-1], "g-", label="Recall")
+    plt.ylabel("Score")
+    plt.xlabel("Decision Threshold")
+    plt.legend(loc='best')
+
+    
+# use the same p, r, thresholds that were previously calculated
+plot_precision_recall_vs_threshold(p, r, thresholds)    
+    
+    
+    
+    
