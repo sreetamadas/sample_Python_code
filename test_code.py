3c7fc81783356d6b95f07afbdf5c041f8202587e
sreetamadas <sreetama.d2005@gmail.com>
2020-01-30 18:06:02 +0530

    Update test_code.py

--- test_code.py
+++ test_code.py
@@ -176,30 +176,6 @@ except:
    #break
 
 
-#######################################################################################
-##### declaring functions #####
- def function_name (comma-separated list of arguments):
-	"optional documentation string"
-	block of code
-
-#def addMe2Me (x):
-#	"apply + operation to argument"
-#	return (x + x)
-#x = [1, 'a']  # 'abc'
-#y = addMe2Me(x)
-#print y
-
-
-#####################################################################################
-
-## some built-in functions ###
-
-# range([start,]stop[,step])	
-# gives list of integers, begin at 'start' up to (but not including) 'stop' in increments of 'step'; default => start=0, step = 1
-
-
-###############################################################################################################
-
 
 #############################################################################################################
 ## NOTE: python codes are delimited by indentation. indentation should be same for all codes in a block (else, error)
