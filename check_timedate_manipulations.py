a2673cbfe2777d7d0b2ec5eb9635b37ec5066202
sreetamadas <sreetama.d2005@gmail.com>
2020-01-30 17:00:35 +0530

    Update check_timedate_manipulations.py

--- check_timedate_manipulations.py
+++ check_timedate_manipulations.py
@@ -16,6 +16,11 @@ df['Date'] = df['Date'].dt.strftime("%Y-%m-%d")
 mc['Day'] = [d.date() for d in mc['Date']]  # d.date() for date & d.time for time part
 
 
+## extract portion from dateTime
+df.loc[row_index, 'hr'] = df.loc[row_index, 'dateTime'].hour
+
+
+
 #energy['Day'] = pandas.to_datetime(energy['Day']) 
 energy['day_of_week2'] = energy['Day'].dt.weekday_name
 energy['day_of_week'] = energy['Day'].dt.dayofweek
@@ -28,12 +33,20 @@ df["dateUTC"] = pd.to_datetime(df["dateUTC"]) #, format="%Y.%m.%d")  ## specify 
 from datetime import datetime, timedelta
 df['dateTime'] = df['dateUTC'] + timedelta(minutes=330)  ## fix dateTime to IST from UTC : add 5 hrs 30 min
 
+
+# calculate difference/ interval b/w time stamps
+#df.loc[row_index, 'timedel'] =  (df.loc[row_index, 'dateTime'] - df.loc[(row_index-1), 'dateTime']) #.astype('timedelta64[m]')
+df.loc[row_index, 'timedel'] =  pd.Timedelta(pd.Timestamp(df.loc[row_index, 'dateTime']) - pd.Timestamp(df.loc[(row_index-1), 'dateTime'])).total_seconds()/60 #.astype('timedelta64[m]')
+
+
+
 # insert missing time stamps (for data missed after operating hrs)
 # fill in missing values with previous value
 # 1. Set the datestamp columns as the index of your DataFrame
 df = df.set_index('dateTime')
 df = df.reindex(pd.date_range(start=df.index[0], end=df.index[-1], freq='300s'))
 df = df.fillna(method='ffill')
+
 
 # set dateTime as a separate column instead of index
 df['dateTime'] = df.index
