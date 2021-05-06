de35f2a8c6a8cc0e0ea61d51f1646c6d3e048938
sreetamadas <sreetama.d2005@gmail.com>
2021-05-06 19:48:25 +0530

    Update math_calc.py

--- math_calc.py
+++ math_calc.py
@@ -9,3 +9,14 @@ y = math.pow(0.983,(math.exp(0.9 * total)))
 
 # calculate sqrt
 rmse = math.sqrt(np.mean((y_pred - y_actual) ** 2))
+
+
+# rolling median
+df['median'] = df['y'].rolling(window=30, center=True).median().fillna(method='bfill').fillna(method='ffill')
+
+
+# calculate median
+df.loc[0,'i1'] = pd.DataFrame.median(data_file.iloc[:,2] - data_file.iloc[:,3])#.median()
+
+# 
+x.loc[i,'d'] = math.log(I1)-math.log(abs(x.loc[i,'i1']))
