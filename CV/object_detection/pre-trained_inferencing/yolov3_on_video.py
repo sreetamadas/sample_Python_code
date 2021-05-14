e900fc79ae27087e022fc725f96aee1ddc45b0ba
sreetamadas <sreetama.d2005@gmail.com>
2021-05-14 18:45:38 +0530

    Create yolov3_on_video.py

---
+++ CV/object_detection/pre-trained_inferencing/yolov3_on_video.py
@@ -0,0 +1,203 @@
+import os
+import time
+import pandas as pd
+import numpy as np
+import argparse
+import imutils
+import time
+import cv2
+
+
+# define the labels (used for marking boxes on output vid)
+LABELS = ["person", "bicycle", "car", "motorbike", "aeroplane", "bus", "train", "truck",
+	"boat", "traffic light", "fire hydrant", "stop sign", "parking meter", "bench",
+	"bird", "cat", "dog", "horse", "sheep", "cow", "elephant", "bear", "zebra", "giraffe",
+	"backpack", "umbrella", "handbag", "tie", "suitcase", "frisbee", "skis", "snowboard",
+	"sports ball", "kite", "baseball bat", "baseball glove", "skateboard", "surfboard",
+	"tennis racket", "bottle", "wine glass", "cup", "fork", "knife", "spoon", "bowl", "banana",
+	"apple", "sandwich", "orange", "broccoli", "carrot", "hot dog", "pizza", "donut", "cake",
+	"chair", "sofa", "pottedplant", "bed", "diningtable", "toilet", "tvmonitor", "laptop", "mouse",
+	"remote", "keyboard", "cell phone", "microwave", "oven", "toaster", "sink", "refrigerator",
+	"book", "clock", "vase", "scissors", "teddy bear", "hair drier", "toothbrush"]
+
+# initialize a list of colors to represent each possible class label
+np.random.seed(42)
+COLORS = np.random.randint(0, 255, size=(len(LABELS), 3), dtype="uint8")
+
+
+
+# derive the paths to the YOLO weights and model configuration
+lpath = "./check_CUDA/coco_yolo/"
+weightsPath = lpath+"yolov3.weights"
+configPath = lpath+"yolov3.cfg"
+
+input_conf = 0.4  # class_thres
+input_thres = 0.4  # nms_thres
+input_w, input_h = 416, 416
+
+
+
+# loading model
+net = cv2.dnn.readNetFromDarknet(configPath, weightsPath)
+# set up to access CUDA (OPTIONAL)
+#net.setPreferableBackend(cv2.dnn.DNN_BACKEND_CUDA)
+#net.setPreferableTarget(cv2.dnn.DNN_TARGET_CUDA)
+
+# determine only the *output* layer names that we need from YOLO
+ln = net.getLayerNames()
+ln = [ln[i[0] - 1] for i in net.getUnconnectedOutLayers()]
+
+
+
+# I/O
+input_video = './check_CUDA/sample3.avi' 
+out_vid = "./check_CUDA/yolo_out.avi"
+
+
+
+#  initialize the video stream, pointer to output video file, and
+# frame dimensions
+vs = cv2.VideoCapture(input_video)
+writer = None
+(W, H) = (None, None)
+
+# try to determine the total number of frames in the video file
+try:
+	prop = cv2.cv.CV_CAP_PROP_FRAME_COUNT if imutils.is_cv2() \
+		else cv2.CAP_PROP_FRAME_COUNT
+	total = int(vs.get(prop))
+	print("[INFO] {} total frames in video".format(total))
+
+# an error occurred while trying to determine the total
+# number of frames in the video file
+except:
+	print("[INFO] could not determine # of frames in video")
+	print("[INFO] no approx. completion time can be provided")
+	total = -1
+
+
+
+  
+
+framewise_total_detections = []
+all_results = []
+# loop over frames from the video file stream
+while True:
+	# read the next frame from the file
+	(grabbed, frame) = vs.read()
+
+	# if the frame was not grabbed, then we have reached the end
+	# of the stream
+	if not grabbed:
+		break
+
+	# if the frame dimensions are empty, grab them
+	if W is None or H is None:
+		(H, W) = frame.shape[:2]
+
+	# construct a blob from the input frame and then perform a forward
+	# pass of the YOLO object detector, giving us our bounding boxes
+	# and associated probabilities
+	blob = cv2.dnn.blobFromImage(frame, 1 / 255.0, (input_w, input_h),
+		swapRB=True, crop=False)
+	net.setInput(blob)
+	start = time.time()
+	layerOutputs = net.forward(ln)
+	end = time.time()
+
+	# initialize our lists of detected bounding boxes, confidences,
+	# and class IDs, respectively
+	boxes = []
+	confidences = []
+	classIDs = []
+
+	# loop over each of the layer outputs
+	for output in layerOutputs:
+		# loop over each of the detections
+		for detection in output:
+			# extract the class ID and confidence (i.e., probability)
+			# of the current object detection
+			scores = detection[5:]
+			classID = np.argmax(scores)
+			confidence = scores[classID]
+
+			# filter out weak predictions by ensuring the detected
+			# probability is greater than the minimum probability
+			if confidence > input_conf and classID==0:  # selecting only people class
+				# scale the bounding box coordinates back relative to
+				# the size of the image, keeping in mind that YOLO
+				# actually returns the center (x, y)-coordinates of
+				# the bounding box followed by the boxes' width and
+				# height
+				box = detection[0:4] * np.array([W, H, W, H])
+				(centerX, centerY, width, height) = box.astype("int")
+
+				# use the center (x, y)-coordinates to derive the top
+				# and and left corner of the bounding box
+				x = int(centerX - (width / 2))
+				y = int(centerY - (height / 2))
+
+				# update our list of bounding box coordinates,
+				# confidences, and class IDs
+				boxes.append([x, y, int(width), int(height)])
+				confidences.append(float(confidence))
+				classIDs.append(classID)
+
+	# apply non-maxima suppression to suppress weak, overlapping
+	# bounding boxes
+	idxs = cv2.dnn.NMSBoxes(boxes, confidences, input_conf,
+                         input_thres) #args["confidence"], args["threshold"])
+
+	framewise_total_detections.append([len(idxs)])
+    # ensure at least one detection exists
+	if len(idxs) > 0:
+		# loop over the indexes we are keeping
+		for i in idxs.flatten():
+			# extract the bounding box coordinates
+			(x, y) = (boxes[i][0], boxes[i][1])
+			(w, h) = (boxes[i][2], boxes[i][3])
+      
+			all_results.append([count, x, y, x+w, y+h, '%.3f'%confidences[i]])
+#			s = str(classIDs[i]) + ' ' + str('%.5f'%confidences[i]) + ' ' + str(x) + ' ' + str(y) + ' ' + str(x+w) + ' ' + str(y+h) + "\n"
+
+			# draw a bounding box rectangle and label on the frame
+			color = [int(c) for c in COLORS[classIDs[i]]]
+			cv2.rectangle(frame, (x, y), (x + w, y + h), color, 2)
+			#text = "{}: {:.4f}".format(LABELS[classIDs[i]],
+			#	confidences[i])
+			#cv2.putText(frame, text, (x, y - 5),
+			#	cv2.FONT_HERSHEY_SIMPLEX, 0.5, color, 2)
+
+	# check if the video writer is None
+	if writer is None:
+		# initialize our video writer
+		fourcc = cv2.VideoWriter_fourcc(*"MJPG")
+		writer = cv2.VideoWriter(out_vid, fourcc, 30,
+			(frame.shape[1], frame.shape[0]), True)
+
+		# some information on processing single frame
+		if total > 0:
+			elap = (end - start)
+			print("[INFO] single frame took {:.4f} seconds".format(elap))
+			print("[INFO] estimated total time to finish: {:.4f}".format(
+				elap * total))
+
+	# write the output frame to disk
+	writer.write(frame)
+
+
+# release the file pointers (after writing output
+print("[INFO] cleaning up...")
+writer.release()
+vs.release()
+
+
+import pandas as pd
+framewise_total_detections = pd.DataFrame(framewise_total_detections, columns =['num_people'])
+all_results = pd.DataFrame(results, columns = ['frameNumber', 'xmin', 'ymin', 'xmax', 'ymax', 'score'])
+
+
+
+
+
+
